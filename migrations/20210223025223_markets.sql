8c96a68dd481df675e0a21ca84b2857662ab85e0
Gabriel Comte <comte@puzzle.ch>
2021-08-13 16:06:40 +0200

    Remove Ethereum structure and data (rollups, signatures, token-address) from migrations, add multi-coin data

--- migrations/20210223025223_markets.sql
+++ migrations/20210223025223_markets.sql
@@ -3,15 +3,8 @@ CREATE TABLE asset (
     id VARCHAR(64) NOT NULL PRIMARY KEY,
     symbol VARCHAR(30) NOT NULL DEFAULT '',
     name VARCHAR(30) NOT NULL DEFAULT '',
-    chain_id SMALLINT CHECK (chain_id >= 0) NOT NULL DEFAULT 1, -- we actually only have one same chain_id for all records
-    token_address VARCHAR(64) NOT NULL DEFAULT '',
-    rollup_token_id integer CHECK (rollup_token_id >= 0) NOT NULL,
-    -- token_address VARCHAR(64) DEFAULT NULL,
-    -- UNIQUE (chain_id, token_address),
-    UNIQUE (chain_id, rollup_token_id),
     precision_stor SMALLINT CHECK (precision_stor >= 0) NOT NULL,
     precision_show SMALLINT CHECK (precision_show >= 0) NOT NULL,
-    logo_uri VARCHAR(256) NOT NULL DEFAULT '',
     create_time TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP
 );
 
