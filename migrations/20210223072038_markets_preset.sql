ef08891044b57df7f9521f4b572afb4aaedb98d5
Gabriel Comte <comte@puzzle.ch>
2021-08-15 23:20:19 +0200

    Add BitRAW-specific market presets

--- migrations/20210223072038_markets_preset.sql
+++ migrations/20210223072038_markets_preset.sql
@@ -1,17 +1,14 @@
 -- Add migration script here
 
 insert into asset (id, symbol, name, precision_stor, precision_show) values
-	('BTC', 'BTC', 'Bitcoin', 6, 6),
-	('ETH', 'ETH', 'Ether', 6, 6),
-	('LTC', 'LTC', 'Litecoin', 6, 6),
-	('XMR', 'XMR', 'Monero', 6, 6),
-	('USDT', 'USDT', 'Tether USD', 6, 6)
-	;
+    ('BTC', 'BTC', 'Bitcoin', 6, 6),
+    ('DIF', 'DIF', 'Difficulty', 6, 6),
+    ('FEE', 'FEE', 'Median Fee', 6, 6)
+    ;
 
 -- Fee is disabled
 insert into market (base_asset, quote_asset, precision_amount, precision_price, precision_fee, min_amount) values
-	('BTC', 'USDT', 4, 2, 0, 0.001),
-	('ETH', 'USDT', 4, 2, 0, 0.001),
-	('LTC', 'USDT', 4, 2, 0, 0.001),
-	('XMR', 'USDT', 4, 2, 0, 0.001)
-	;
+    ('BTC', 'DIF', 4, 2, 0, 0.001),
+    ('BTC', 'FEE', 4, 2, 0, 0.001)
+    ;
+
