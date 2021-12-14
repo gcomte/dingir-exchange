7e0917065fff07915af4f16e502811eceb9b369b
Gabriel Comte <comte@puzzle.ch>
2021-12-15 00:09:04 +0100

    Add authorization tests for REST interface

--- examples/js/tests/authorization.ts
+++ examples/js/tests/authorization.ts
@@ -1,128 +1,191 @@
 import { TestUser } from "../config"; // dotenv
-import { defaultClient as client } from "../client";
+import { defaultClient as grpcClient } from "../client";
+import { defaultRESTClient as restClient } from "../RESTClient";
 import * as assert from "assert";
+import axios from "axios";
 
-const PERMISSION_DENIED_CODE = 7;
-const PERMISSION_DENIED_TEXT = "Requires admin role.";
-const TOKEN_NOT_FOUND_CODE = 16;
-const TOKEN_NOT_FOUND_TEXT = "Token not found";
-const INVALID_TOKEN_CODE = 16;
-const INVALID_TOKEN_TEXT = "InvalidToken";
-const INVALID_SIGNATURE_CODE = 16;
-const INVALID_SIGNATURE_TEXT = "InvalidSignature";
-const TOKEN_EXPIRED_CODE = 16;
-const TOKEN_EXPIRED_TEXT = "ExpiredSignature";
+const GRPC_PERMISSION_DENIED_CODE = 7;
+const GRPC_PERMISSION_DENIED_TEXT = "Requires admin role.";
+const GRPC_TOKEN_NOT_FOUND_CODE = 16;
+const GRPC_TOKEN_NOT_FOUND_TEXT = "Token not found";
+const GRPC_INVALID_TOKEN_CODE = 16;
+const GRPC_INVALID_TOKEN_TEXT = "InvalidToken";
+const GRPC_INVALID_SIGNATURE_CODE = 16;
+const GRPC_INVALID_SIGNATURE_TEXT = "InvalidSignature";
+const GRPC_TOKEN_EXPIRED_CODE = 16;
+const GRPC_TOKEN_EXPIRED_TEXT = "ExpiredSignature";
 
-async function permitAdminAccess() {
+const HTTP_UNAUTHORIZED_CODE = 401;
+const HTTP_UNAUTHORIZED_TEXT = "Unauthorized";
+
+async function grpcPermitAdminAccess() {
   // should be executed without throwing any errors.
-  await client.reloadMarkets(TestUser.ADMIN);
-  await client.debugReset(TestUser.ADMIN);
-  await client.debugReload(TestUser.ADMIN);
-  await client.debugDump(TestUser.ADMIN);
+  await grpcClient.reloadMarkets(TestUser.ADMIN);
+  await grpcClient.debugReset(TestUser.ADMIN);
+  await grpcClient.debugReload(TestUser.ADMIN);
+  await grpcClient.debugDump(TestUser.ADMIN);
 }
 
-async function rejectUserAccessingAdminCalls() {
+async function grpcRejectUserAccessingAdminCalls() {
   try {
-    await client.reloadMarkets(TestUser.USER1);
+    await grpcClient.reloadMarkets(TestUser.USER1);
     throw Error("Non-admin must not be able to call Admin Remote Procedures!");
   } catch (e) {
-    assertErrorPermissionDenied(e);
+    grpcAssertErrorPermissionDenied(e);
   }
 
   try {
-    await client.debugReset(TestUser.USER1);
+    await grpcClient.debugReset(TestUser.USER1);
     throw Error("Non-admin must not be able to call Admin Remote Procedures!");
   } catch (e) {
-    assertErrorPermissionDenied(e);
+    grpcAssertErrorPermissionDenied(e);
   }
 
   try {
-    await client.debugReload(TestUser.USER1);
+    await grpcClient.debugReload(TestUser.USER1);
     throw Error("Non-admin must not be able to call Admin Remote Procedures!");
   } catch (e) {
-    assertErrorPermissionDenied(e);
+    grpcAssertErrorPermissionDenied(e);
   }
 
   try {
-    await client.debugDump(TestUser.USER1);
+    await grpcClient.debugDump(TestUser.USER1);
     throw Error("Non-admin must not be able to call Admin Remote Procedures!");
   } catch (e) {
-    assertErrorPermissionDenied(e);
+    grpcAssertErrorPermissionDenied(e);
   }
 }
 
-async function rejectUserWithoutToken() {
+async function grpcRejectUserWithoutToken() {
   try {
-    await client.balanceQueryWithoutJWT();
+    await grpcClient.balanceQueryWithoutJWT();
     throw Error("GRPC call must fail as no authentication token is provided!");
   } catch (e) {
-    assertErrorNoTokenProvided(e);
+    grpcAssertErrorNoTokenProvided(e);
   }
 }
 
-async function rejectUserWithInvalidToken() {
+async function grpcRejectUserWithInvalidToken() {
   try {
-    await client.balanceQueryWithInvalidToken();
+    await grpcClient.balanceQueryWithInvalidToken();
     throw Error("GRPC call must fail as authentication token is invalid!");
   } catch (e) {
-    assertErrorInvalidToken(e);
+    grpcAssertErrorInvalidToken(e);
   }
 }
 
-async function rejectUserWithInvalidSignatureToken() {
+async function grpcRejectUserWithInvalidSignatureToken() {
   try {
-    await client.balanceQueryWithInvalidSignatureToken();
+    await grpcClient.balanceQueryWithInvalidSignatureToken();
     throw Error("GRPC call must fail as the authentication token's signature doesn't check out!");
   } catch (e) {
-    assertErrorInvalidTokenSignature(e);
+    grpcAssertErrorInvalidTokenSignature(e);
   }
 }
 
-async function rejectUserWithEpiredToken() {
+async function grpcRejectUserWithEpiredToken() {
   try {
-    await client.balanceQueryWithExpiredToken();
-    throw Error("GRPC call must fail as the authentication token's signature doesn't check out!");
+    await grpcClient.balanceQueryWithExpiredToken();
+    throw Error("GRPC call must fail as the authentication token is expired!");
   } catch (e) {
-    assertErrorExpiredToken(e);
+    grpcAssertErrorExpiredToken(e);
   }
 }
 
-function assertErrorPermissionDenied(error) {
-  assert.equal(error.code, PERMISSION_DENIED_CODE);
-  assert.equal(error.details, PERMISSION_DENIED_TEXT);
+async function restRejectUserWithoutToken() {
+  try {
+    await restClient.closed_orders("");
+    throw Error("REST call must fail as no authentication token is provided!");
+  } catch (e) {
+    restAssertErrorNoTokenProvided(e);
+  }
 }
 
-function assertErrorNoTokenProvided(error) {
-  assert.equal(error.code, TOKEN_NOT_FOUND_CODE);
-  assert.equal(error.details, TOKEN_NOT_FOUND_TEXT);
+async function restRejectUserWithInvalidToken() {
+  try {
+    await restClient.closed_orders("LoremIpsum");
+    throw Error("REST call must fail as authentication token is invalid!");
+  } catch (e) {
+    restAssertErrorNoTokenProvided(e);
+  }
 }
 
-function assertErrorInvalidToken(error) {
-  assert.equal(error.code, INVALID_TOKEN_CODE);
-  assert.equal(error.details, INVALID_TOKEN_TEXT);
+async function restRejectUserWithInvalidToken2() {
+  try {
+    await restClient.closed_orders("Bearer LoremIpsum");
+    throw Error("REST call must fail as authentication token is invalid!");
+  } catch (e) {
+    restAssertErrorNoTokenProvided(e);
+  }
 }
 
-function assertErrorInvalidTokenSignature(error) {
-  assert.equal(error.code, INVALID_SIGNATURE_CODE);
-  assert.equal(error.details, INVALID_SIGNATURE_TEXT);
+async function restRejectUserWithInvalidSignatureToken() {
+  try {
+    await restClient.closed_orders(process.env.JWT_INVALID_SIGNATURE);
+    throw Error("REST call must fail as the authentication token's signature doesn't check out!");
+  } catch (e) {
+    restAssertErrorNoTokenProvided(e);
+  }
 }
 
-function assertErrorExpiredToken(error) {
-  assert.equal(error.code, TOKEN_EXPIRED_CODE);
-  assert.equal(error.details, TOKEN_EXPIRED_TEXT);
+async function restRejectUserWithEpiredToken() {
+  try {
+    await restClient.closed_orders(process.env.JWT_EXPIRED);
+    throw Error("REST call must fail as the authentication token is expired!");
+  } catch (e) {
+    restAssertErrorNoTokenProvided(e);
+  }
+}
+
+function grpcAssertErrorPermissionDenied(error) {
+  assert.equal(error.code, GRPC_PERMISSION_DENIED_CODE);
+  assert.equal(error.details, GRPC_PERMISSION_DENIED_TEXT);
+}
+
+function grpcAssertErrorNoTokenProvided(error) {
+  assert.equal(error.code, GRPC_TOKEN_NOT_FOUND_CODE);
+  assert.equal(error.details, GRPC_TOKEN_NOT_FOUND_TEXT);
+}
+
+function grpcAssertErrorInvalidToken(error) {
+  assert.equal(error.code, GRPC_INVALID_TOKEN_CODE);
+  assert.equal(error.details, GRPC_INVALID_TOKEN_TEXT);
+}
+
+function grpcAssertErrorInvalidTokenSignature(error) {
+  assert.equal(error.code, GRPC_INVALID_SIGNATURE_CODE);
+  assert.equal(error.details, GRPC_INVALID_SIGNATURE_TEXT);
+}
+
+function grpcAssertErrorExpiredToken(error) {
+  assert.equal(error.code, GRPC_TOKEN_EXPIRED_CODE);
+  assert.equal(error.details, GRPC_TOKEN_EXPIRED_TEXT);
+}
+
+function restAssertErrorNoTokenProvided(error) {
+  assert.equal(error.response.status, HTTP_UNAUTHORIZED_CODE);
+  assert.equal(error.response.statusText, HTTP_UNAUTHORIZED_TEXT);
 }
 
 async function main() {
   try {
-    await permitAdminAccess();
-    await rejectUserAccessingAdminCalls();
-    await rejectUserWithoutToken();
-    await rejectUserWithInvalidToken();
-    await rejectUserWithInvalidSignatureToken();
-    await rejectUserWithEpiredToken();
+    console.log("GRPC authentication");
+    await grpcPermitAdminAccess();
+    await grpcRejectUserAccessingAdminCalls();
+    await grpcRejectUserWithoutToken();
+    await grpcRejectUserWithInvalidToken();
+    await grpcRejectUserWithInvalidSignatureToken();
+    await grpcRejectUserWithEpiredToken();
+    console.log("REST authentication");
+    await restRejectUserWithoutToken();
+    await restRejectUserWithInvalidToken();
+    await restRejectUserWithInvalidToken2();
+    await restRejectUserWithInvalidSignatureToken();
+    await restRejectUserWithEpiredToken();
+
     console.log("Authorization tests successful!");
   } catch (error) {
-    console.error("Caught error:", error);
+    // console.error("Caught error:", error);
     process.exit(1);
   }
 }
